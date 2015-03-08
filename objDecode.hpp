1e550bd1c02486f61dcdc8187a8fe2574a056fed
Jagdeep Manik <jagdeepmanik@gmail.com>
2015-03-08 00:10:45 -0800

    obj
    obj stuff

--- objDecode.hpp
+++ objDecode.hpp
@@ -1,4 +1,24 @@
+class Vertex {
+	float x; float y; float z;
+};
 
+class VertexTexture {
+	float u; float v; float w;
+};
+
+class VertexNormal {
+	float x; float y; float z;
+};
+
+class VertexParameter {
+	float u; float v; float w;
+};
+
+class OBJ {
+public:
+	std::vector<Vertex> vertices; 
+	std::vector
+};
 
 class ObjParseFile
 ObjParsedFile* objDecode(char* filename);
