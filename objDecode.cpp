54419dd41672f0ca1c0ae29c02d8d60b28e9b3f1
Jagdeep Manik <jagdeepmanik@gmail.com>
2015-03-08 04:47:30 -0700

    added more parsing
    decoded stuff, supports faces (untested) and sjdkafl

--- objDecode.cpp
+++ objDecode.cpp
@@ -10,6 +10,16 @@
 #include <string>
 #include <sstream>
 
+#define VERTEX_DEFAULT		0
+#define VERTEX_NORMAL		1
+#define VERTEX_TEXTURE		2
+#define VERTEX_PARAMETER	3
+
+#define FACE_V 		0
+#define FACE_VVT 	1
+#define FACE_VVTN 2
+#define FACE_VN  	3
+
 // trim from start
 inline std::string &ltrim(std::string &s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
@@ -17,11 +27,7 @@ inline std::string &ltrim(std::string &s) {
 }
 /*****************/
 
-bool startsWith(std::string a, std::string start) {
-	return a.compare(0, start.length(), start) == 0;
-}
 
-/***/
 /* http://stackoverflow.com/questions/236129/split-a-string-in-c */
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
     std::stringstream ss(s);
@@ -40,18 +46,61 @@ std::vector<std::string> split(const std::string &s, char delim) {
 }
 /***/
 
-void storeVertex(OBJ* decoded, std::vector<std::string> tokens) {
+void storeVertex(OBJ* decoded, std::vector<std::string> tokens, int type) {
 	if (tokens.size() <= 1) { return; }
 	Vertex v;
-	int j = 0;
+	v.dimension = 0;
+	for (int i = 1; i < tokens.size(); i++) {
+		if (tokens[i].compare(" ") == 0) { continue; }
+		if ((tokens[i].compare("#") == 0) || (v.dimension == 3)) { break; }
+		if (v.dimension == 0) { v.x = std::stof(tokens[i]); v.dimension += 1; }
+		else if (v.dimension == 1) { v.y = std::stof(tokens[i]); v.dimension += 1; }
+		else if (v.dimension == 2) { v.z = std::stof(tokens[i]); v.dimension += 1; }
+	}
+	if (type == VERTEX_DEFAULT) { decoded->v.push_back(v); }
+	else if (type == VERTEX_NORMAL)  { decoded->vn.push_back(v); }
+	else if (type == VERTEX_TEXTURE) { decoded->vt.push_back(v); }
+	else if (type == VERTEX_PARAMETER) { decoded->vp.push_back(v); }
+}
+
+void storeFace(OBJ* decoded, std::vector<std::string> tokens) {
+	if (tokens.size() <= 1) { return; }
+	Face f;
+	Vertex v;
 	for (int i = 1; i < tokens.size(); i++) {
 		if (tokens[i].compare(" ") == 0) { continue; }
 		if (tokens[i].compare("#") == 0) { break; }
-		if (j == 0) { v.x = std::stof(tokens[i]); j += 1; }
-		else if (j == 1) { v.y = std::stof(tokens[i]); j += 1; }
-		else if (j == 2) { v.z = std::stof(tokens[i]); j += 1; }
+		size_t find = tokens[i].find('/');
+		if (find == std::string::npos) {
+			v = decoded->v[decoded->v.size() - std::stoi(tokens[i])];
+			f.v.push_back(v);
+			f.type = FACE_V;
+		}
+		else if ((find != std::string::npos) && (tokens[i].substr(find, find+1).compare("/") == 0)) {
+			std::vector<std::string> subtokens = split(tokens[i], char(47));
+			v = decoded->v[decoded->v.size() - std::stoi(subtokens[0])];
+			f.v.push_back(v);
+			f.type = FACE_VN;
+			v = decoded->vt[decoded->v.size() - std::stoi(subtokens[2])];
+			f.vn.push_back(v);
+		}
+		else if (find != std::string::npos) {
+			std::vector<std::string> subtokens = split(tokens[i], char(47));
+			v = decoded->v[decoded->v.size() - std::stoi(subtokens[0])];
+			f.v.push_back(v);
+			v.print(); // debug
+			f.type = FACE_V;
+			if (subtokens.size() >= 2) {
+				v = decoded->vt[decoded->v.size() - std::stoi(subtokens[1])];
+				f.vt.push_back(v); f.type = FACE_VVT; 
+			}
+			if (subtokens.size() >= 3) { 
+				v = decoded->vt[decoded->v.size() - std::stoi(subtokens[2])];
+				f.vn.push_back(v); f.type = FACE_VVTN;
+			}
+		}
 	}
-	decoded->v.push_back(v);
+	decoded->faces.push_back(f);
 }
 
 
@@ -64,9 +113,11 @@ OBJ* OBJ::decodeObj(std::string filename){
 			line = ltrim(line);
 			std::vector<std::string> tokens = split(line, char(32));
 			if (tokens.size() == 0) { continue; }
-			if (tokens[0].compare("v") == 0) {
-				storeVertex(decoded, tokens);
-			}
+			else if (tokens[0].compare("v") == 0) { storeVertex(decoded, tokens, VERTEX_DEFAULT); }
+			else if (tokens[0].compare("vt") == 0) { storeVertex(decoded, tokens, VERTEX_TEXTURE); }
+			else if (tokens[0].compare("vn") == 0) { storeVertex(decoded, tokens, VERTEX_NORMAL); }
+			else if (tokens[0].compare("vp") == 0) { storeVertex(decoded, tokens, VERTEX_PARAMETER); }
+			else if (tokens[0].compare("f") == 0) { storeFace(decoded, tokens); }
 		}
 		objIn.close();
 	}
@@ -82,8 +133,17 @@ void OBJ::printVertices() {
 	}
 }
 
+void OBJ::printFaces() {
+	if (faces.size() > 0) {
+		for (int i = 0; i < faces.size(); i++) {
+			faces[i].print();
+		}
+	}
+}
+
 int main(int argc, char** argv){
 	OBJ* abc = OBJ::decodeObj("sample/cube.obj");
-	abc->printVertices();
+	//abc->printVertices();
+	abc->printFaces();
 	return 0;
 }
--- objDecode.hpp
+++ objDecode.hpp
@@ -3,12 +3,13 @@
 
 class Vertex {
 public:
+	int dimension;
 	float x; float y; float z;
 
 	void print() { std::cout << "Vertex: ( " << x << " , " << y << " , " << z << " )" << std::endl; }
 };
 
-class VertexTexture {
+/* class VertexTexture {
 public:
 	float u; float v; float w;
 };
@@ -21,21 +22,37 @@ public:
 class VertexParameter {
 public:
 	float u; float v; float w;
-};
+}; */
 
 class Face {
 public:
+	int type;
 	std::vector<Vertex> v;
+	std::vector<Vertex> vt;
+	std::vector<Vertex> vn;
+	void print() {
+		std::cout << "Face: " << std::endl;
+		for (int i = 0; i < v.size(); i++) {
+			v[i].print();
+		}
+		for (int i = 0; i < vt.size(); i++) {
+			vt[i].print();
+		}
+		for (int i = 0; i < vn.size(); i++) {
+			vn[i].print();
+		}
+	};
 };
 
 class OBJ {
 public:
 	std::vector<Vertex> v; 
-	std::vector<VertexTexture> vt;
-	std::vector<VertexNormal> vn;
-	std::vector<VertexParameter> vp;
+	std::vector<Vertex> vt;
+	std::vector<Vertex> vn;
+	std::vector<Vertex> vp;
 	std::vector<Face> faces;
 
 	static OBJ* decodeObj(std::string filename);
 	void printVertices();
+	void printFaces();
 };
